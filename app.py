72946135947f215aeb3a04076f15d52e75c01e92
itsuki-deve1oper <yxvanr@gmail.com>
2026-02-04 17:21:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+# 例：Flaskを使う場合
+from flask import Flask
+import os
 
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, Render!"
+
+if __name__ == "__main__":
+    # Renderが指定するポート番号を読み込む（超重要！）
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
